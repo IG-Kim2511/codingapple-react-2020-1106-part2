c1a68076559c06880ebee5adee8acf428b0439f2
IG-Kim <iikim2511@gmail.com>
2020-11-07 16:17:36 -0500

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App15(){
+  return(
+    <div>
     </div>
-  );
+  )
+
+}
+
+//
+function App55(){
+  return(
+    <div>
+    </div>
+  )
+
+}
+
+function App(){
+  return(
+    <div>
+       <App15/>      
+       
+       </div>
+       //  <App16/>      
+       //  <App17/>      
+       //  <App18/>    
+       //  <App19/>    
+       //  <App20/>    
+       //  <App21/>    
+       //  <App22/>    
+       //  <App23/>    
+       //  <App24/>    
+       //  <App25/>    
+       //  <App26/>    
+       //  <App27/>    
+       //  <App28/>    
+       //  <App30/>    
+       //  <App31/>    
+       //  <App32/>    
+       //  <App33/>    
+       //  <App34/>    
+       //  <App35/>   
+       )
 }
 
 export default App;
